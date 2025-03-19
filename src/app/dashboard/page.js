74532ea82ec0df95ba2347819e7bedb85ef5de41
John Huang <pheno_the_best@yahoo.com.tw>
2025-03-19 17:25:03 +0800

    remove hamburger icon

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -165,22 +165,6 @@ export default function Dashboard() {
               </span>
             </div>
           </div>
-          <div className="flex items-center gap-4">
-            <button className="p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg transition-colors">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
         </div>
 
         {/* Current Plan Card */}
